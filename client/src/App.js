39f64fff146a0d7277643f38e55deb8155dc783c
sahil505 <sahilkhokhar505@gmail.com>
2019-11-09 20:20:47 +0900

    basic frontend layout created

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,15 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import FileUpload from "./components/FileUpload";
+import "./App.css";
 
-const App = () => <div className='App'>app</div>
+const App = () => (
+  <div className="container mt-4">
+    <h4 className="display-4 text-center mb-4">
+      <i className="fas fa-music"></i> Cap That Pic
+    </h4>
+
+    <FileUpload />
+  </div>
+);
 
 export default App;
