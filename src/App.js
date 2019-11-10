3e6421f183897f88e24a217e21b74cf90a7baf86
aniket1743 <aniketchoudhary1743@gmail.com>
2019-11-10 19:13:32 +0900

    Checking Automatic Deployment...

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn App Deployment
+          Learn Auto Deployment
         </a>
       </header>
     </div>
