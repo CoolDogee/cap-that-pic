fee442cc9ef6bb1719fcc8208836c67eeb24e6e2
sahil505 <sahilkhokhar505@gmail.com>
2019-11-12 20:27:01 +0900

    testing deployment integration

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,9 @@ const App = () => (
     <h4 className="display-4 text-center mb-4">
       <i className="fas fa-music"></i> Cap That Pic
     </h4>
+    <h5 className="display-8 text-center mb-4">
+      Every picture deserves a perfect caption
+    </h5>
     <FileUpload />
   </div>
 );
