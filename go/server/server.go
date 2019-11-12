3f1b919c89ee93ff6bd9516fc677c975e0fecec1
rocuku <rocuku@gmail.com>
2019-11-12 13:35:43 +0900

    change port from 8080 to 5000

--- go/server/server.go
+++ go/server/server.go
@@ -21,7 +21,7 @@ func CreateRouter() *gin.Engine {
 // StartServer starts given server, supporting graceful shutdown of the server
 func StartServer(router *gin.Engine) {
 	srv := &http.Server{
-		Addr:    ":8080",
+		Addr:    ":5000",
 		Handler: router,
 	}
 
